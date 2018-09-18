04bab0bec1bde7e209b6489739630293a1379a9b
drewcornatzer <acornatzer@mail.csuchico.edu>
2018-09-17 17:09:38 -0700

    Lucky Number Guessing Game

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Author:
+//Author:Drew Cornatzer
 #include <iostream>
 
 using namespace std;
@@ -8,17 +8,12 @@ int main()
   int guess;
 
   cout<<"Guess a number between 1-10: ";
-  cin>>guess;
+  cin>>guess;   
 
+    if(guess==7){cout<<"YES! Lucky number 7!\n";}
+    else if(guess==6||guess==8){cout<<"Oh, so close!\n";}
+    else{cout<<"Waaay off, sorry.\n";}
 
-  //when they guess correctly (7), display:
-  cout<<"YES! Lucky number 7!\n";
-
-  //when they guess just off by one (either 6 or 8), display:
-  cout<<"Oh, so close!\n";
-
-  //when they guess any other number, display:
-  cout<<"Waaay off, sorry.\n";
 
   return 0;
 }
